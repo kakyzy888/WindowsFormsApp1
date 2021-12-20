aa578c18e36c58788f2f503c252b4e3d335b9df3
egorm <egorm@DESKTOP-B7DPUHA>
2021-12-20 14:37:48 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NumberUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(195, 337);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // NumberUpDown1
+            // 
+            this.NumberUpDown1.Location = new System.Drawing.Point(493, 123);
+            this.NumberUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.NumberUpDown1.Name = "NumberUpDown1";
+            this.NumberUpDown1.Size = new System.Drawing.Size(113, 22);
+            this.NumberUpDown1.TabIndex = 1;
+            this.NumberUpDown1.ValueChanged += new System.EventHandler(this.kolvo_strok);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.GhostWhite;
+            this.label1.Location = new System.Drawing.Point(350, 123);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество переменных:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(493, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Вычислить СДНФ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SDNF_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 22);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Вычислить СКНФ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.SKNF_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(272, 224);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(487, 90);
+            this.label2.TabIndex = 6;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(775, 404);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumberUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Программа вычисления СДНФ и СКНФ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown NumberUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//МИХАЛИШИН ЕГОР ОЛЕГОВИЧ ГР.4301-21
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int Fd = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Columns.Add($"Column{NumberUpDown1.Value}", $"F {NumberUpDown1.Value}");
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //определяет количество строк то есть количество переменных
+        void kolvo_strok(object sender, EventArgs e)
+        {
+                for (int i = 0; i < Math.Pow(2, (Convert.ToInt32(NumberUpDown1.Value) - 1)); i++)
+                {
+                    if (Convert.ToInt32(NumberUpDown1.Value) == 1)
+                    {
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows.Add();
+                    }
+                    else
+                    {
+                        dataGridView1.Rows.Add();
+                    }
+                }
+            Fd = Convert.ToInt32(NumberUpDown1.Value);
+        }
+
+        //вычисление СДНФ
+        void SDNF_Click(object sender, EventArgs e)
+        {
+            string bd = "";
+            string b = "";
+            for(int i = 0; i < Math.Pow(2, (Convert.ToInt32(Fd))); i++)
+            {
+                bd = "";
+                if (Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value) == 1)
+                {
+                    b += "(";
+                    bd = Convert.ToString(i, 2);
+                    if (bd.Length != Convert.ToString(Convert.ToInt32(Math.Pow(2, (Fd))), 2).Length)
+                    {
+                        for(int q = 0; true; q++)
+                        {
+                            if(bd.Length+1 == Convert.ToString(Convert.ToInt32(Math.Pow(2, (Fd))), 2).Length)
+                            {
+                                break;
+                            }
+                            bd = "0" + bd;
+                        }
+                    }
+                    for (int j = 0; j < Fd; j++)
+                    {
+                        if (bd[j] == '0')
+                        {
+                            b += "!X" + (j + 1);
+                        }
+                        else
+                        {
+                            b += "X" + (j + 1);
+                        }
+                        if(j+1 != Fd)
+                        {
+                            b += "";
+                        }   
+                    }
+                    b += ")";
+                }
+                if(i != Math.Pow(2, (Convert.ToInt32(Fd))))
+                {
+                    b += "V";
+                }
+            }
+            for(int i = 0; i<b.Length; i++)
+            {
+                if(b[0] == 'V')
+                {
+                    b = b.Remove(0, 1);
+                }
+                if(b[b.Length-1] == 'V')
+                {
+                    b = b.Remove(b.Length-1, 1);
+                }
+                if(b[i] == 'V' && b[i+1] == 'V')
+                {
+                    b = b.Remove(i, 1);
+                    i -= 1;
+                }
+            }
+            label2.Text =  b; 
+        }
+
+        //вычислениее СКНФ
+        void SKNF_Click(object sender, EventArgs e)
+        {
+            string bd = "";
+            string b = "";
+            for (int i = 0; i < Math.Pow(2, (Convert.ToInt32(Fd))); i++)
+            {
+                bd = "";
+                if (Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value) == 0)
+                {
+                    b += "(";
+                    bd = Convert.ToString(i, 2);
+                    if (bd.Length != Convert.ToString(Convert.ToInt32(Math.Pow(2, (Fd))), 2).Length)
+                    {
+                        for (int q = 0; true; q++)
+                        {
+                            if (bd.Length + 1 == Convert.ToString(Convert.ToInt32(Math.Pow(2, (Fd))), 2).Length)
+                            {
+                                break;
+                            }
+                            bd = "0" + bd;
+                        }
+                    }
+                    for (int j = 0; j < Fd; j++)
+                    {
+                        if (bd[j] == '0')
+                        {
+                            b += "X" + (j + 1);
+                        }
+                        else
+                        {
+                            b += "!X" + (j + 1);
+                        }
+                        if (j + 1 != Fd)
+                        {
+                            b += "V";
+                        }
+                    }
+                    b += ")";
+                }
+                if (i != Math.Pow(2, (Convert.ToInt32(Fd))))
+                {
+                    b += "V";
+                }
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (b[0] == 'V')
+                {
+                    b = b.Remove(0, 1);
+                }
+                if (b[b.Length - 1] == 'V')
+                {
+                    b = b.Remove(b.Length - 1, 1);
+                }
+                if (b[i] == 'V' && b[i + 1] == 'V')
+                {
+                    b = b.Remove(i, 1);
+                    i -= 1;
+                }
+            }
+            label2.Text = b;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        } 
+    }
+}
